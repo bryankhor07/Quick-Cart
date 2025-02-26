365552bfdcbdbd4afc7160a585a88f40cec1b89d
Bryan Khor <bryankhor07@gmail.com>
2025-02-25 20:19:31 -0800

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Auth } from "./ui/auth";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Auth />
+    </div>
+  );
+}
