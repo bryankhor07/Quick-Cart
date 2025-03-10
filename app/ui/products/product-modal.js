063713aaded2058b863913cdbcbb6fa44ac847d7
Bryan Khor <bryankhor07@gmail.com>
2025-03-10 11:13:41 -0700

    Refactored product-modal by creating smaller components to improve modularity.

--- app/ui/products/product-modal.js
+++ app/ui/products/product-modal.js
@@ -46,6 +46,201 @@ const AverageStarRating = ({ averageRating }) => {
   );
 };
 
+const Reviews = ({ reviews, product, user, handleDeleteReview }) => {
+  return (
+    <div className="mt-4 border-2 border-black p-4 rounded-md">
+      <h2 className="text-xl font-medium">Reviews</h2>
+      <div className="mt-2">
+        {reviews.map((review, index) => (
+          <div
+            key={index}
+            className="flex flex-col gap-2 border-b border-gray-300 py-2"
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <p className="text-sm font-semibold">{review.userName}</p>
+                <span className="text-yellow-500 text-sm">
+                  ⭐ {review.rating} / 5
+                </span>
+              </div>
+              <p className="text-gray-800 text-sm">{review.createdAt}</p>
+            </div>
+            <div className="flex flex-row justify-between items-center">
+              <p className="text-gray-600">{review.reviewText}</p>
+              {/* Trash icon for deleting the review */}
+              {user?.uid === review.userId && (
+                <FaTrash
+                  className="text-red-500 cursor-pointer"
+                  onClick={() => handleDeleteReview(product.id, review.id)}
+                />
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const WriteReview = ({
+  showRatingBanner,
+  rating,
+  setRating,
+  reviewText,
+  setReviewText,
+  handleAddReview,
+}) => {
+  return (
+    <div className="mt-4 border-2 border-black p-4 rounded-md">
+      <label
+        htmlFor="review"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Write a review
+      </label>
+      {showRatingBanner && (
+        <NotificationBanner text="Please select a rating before posting your review." />
+      )}
+      <StarRating rating={rating} setRating={setRating} />
+      <div className="mt-1 flex gap-2">
+        <textarea
+          id="review"
+          name="review"
+          placeholder="What should others know about this product?"
+          className="mt-1 block h-20 w-full pl-3 pr-10 py-2 text-base border-2 border-black focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
+          value={reviewText}
+          onChange={(e) => setReviewText(e.target.value)}
+        ></textarea>
+        <div className="flex justify-center items-end">
+          <button
+            className="h-10 bg-blue-500 text-white px-4 py-2 rounded-md"
+            onClick={handleAddReview}
+          >
+            Post
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ProductDetails = ({ product, averageRating }) => {
+  return (
+    <>
+      <img
+        src={product.imageURL}
+        alt={product.name}
+        className="w-full h-48 object-contain rounded-md"
+      />
+      <h2 className="text-2xl font-semibold mt-2">{product.name}</h2>
+      <p className="text-gray-600">{product.description}</p>
+      <p className="text-lg font-bold mt-2">${product.price}</p>
+      <div className="mt-2 flex items-center">
+        <AverageStarRating averageRating={averageRating} />
+      </div>
+    </>
+  );
+};
+
+const StockAndPurchase = ({
+  product,
+  quantity,
+  setQuantity,
+  totalPrice,
+  setTotalPrice,
+  selectedShipping,
+  setSelectedShipping,
+  threeDayArrival,
+  fiveDayArrival,
+  handleAddOrder,
+  showQuantityBanner,
+  showOrderBanner,
+}) => {
+  return (
+    <div className="mt-4 border-2 border-black p-4 rounded-md">
+      <h2 className="text-xl font-medium">
+        {product.stockQuantity > 0 ? "In Stock" : "Out of Stock"}
+      </h2>
+      <label
+        htmlFor="quantity"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Quantity
+      </label>
+      <select
+        id="quantity"
+        name="quantity"
+        className="mt-1 block w-18 pl-3 pr-10 py-2 text-base border-2 border-black focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
+        value={quantity}
+        onChange={(e) => {
+          const selectedQuantity = parseInt(e.target.value, 10);
+          setQuantity(selectedQuantity);
+          setTotalPrice(
+            product.price * selectedQuantity + selectedShipping.price
+          );
+        }}
+      >
+        {[1, 2, 3, 4, 5].map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <div className="mt-4">
+        <h3 className="text-lg font-medium mb-2">Choose a shipping option:</h3>
+        <div className="flex gap-4">
+          {[
+            { date: threeDayArrival, price: 5.99 },
+            { date: fiveDayArrival, price: 2.99 },
+          ].map((option) => (
+            <button
+              key={option.date}
+              className={`border-2 px-4 py-2 rounded-md transition ${
+                selectedShipping.date === option.date.toLocaleDateString()
+                  ? "border-blue-500 bg-blue-100"
+                  : "border-black hover:bg-gray-100"
+              }`}
+              onClick={() => {
+                setSelectedShipping({
+                  date: option.date.toLocaleDateString(),
+                  price: option.price,
+                });
+                setTotalPrice(product.price * quantity + option.price);
+              }}
+            >
+              <p className="font-semibold">
+                {option.date.toLocaleDateString()}
+              </p>
+              <p className="text-gray-600 text-sm">
+                ${option.price.toFixed(2)}
+              </p>
+            </button>
+          ))}
+        </div>
+        <div className="mt-2 flex gap-2">
+          <h3 className="text-lg font-medium">Total Price:</h3>
+          <p className="text-xl font-bold">${totalPrice.toFixed(2)}</p>
+        </div>
+      </div>
+      <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md block">
+        Add to cart
+      </button>
+      <button
+        className="mt-4 bg-orange-500 text-white px-4 py-2 rounded-md"
+        onClick={handleAddOrder}
+      >
+        Buy Now
+      </button>
+      {showQuantityBanner && (
+        <NotificationBanner text="Not enough stock available!" />
+      )}
+      {showOrderBanner && (
+        <NotificationBanner text="Order placed successfully!" />
+      )}
+    </div>
+  );
+};
+
 export default function ProductModal({ product, onClose }) {
   const [reviewText, setReviewText] = useState("");
   const [reviews, setReviews] = useState(product.reviews || []);
@@ -162,171 +357,35 @@ export default function ProductModal({ product, onClose }) {
         >
           &times;
         </span>
-        <img
-          src={product.imageURL}
-          alt={product.name}
-          className="w-full h-48 object-contain rounded-md"
+        <ProductDetails product={product} averageRating={averageRating} />
+        <StockAndPurchase
+          product={product}
+          quantity={quantity}
+          setQuantity={setQuantity}
+          totalPrice={totalPrice}
+          setTotalPrice={setTotalPrice}
+          selectedShipping={selectedShipping}
+          setSelectedShipping={setSelectedShipping}
+          threeDayArrival={threeDayArrival}
+          fiveDayArrival={fiveDayArrival}
+          handleAddOrder={handleAddOrder}
+          showQuantityBanner={showQuantityBanner}
+          showOrderBanner={showOrderBanner}
         />
-        <h2 className="text-2xl font-semibold mt-2">{product.name}</h2>
-        <p className="text-gray-600">{product.description}</p>
-        <p className="text-lg font-bold mt-2">${product.price}</p>
-        <div className="mt-2 flex items-center">
-          <AverageStarRating averageRating={averageRating} />
-        </div>
-        <div className="mt-4 border-2 border-black p-4 rounded-md">
-          <h2 className="text-xl font-medium">
-            {product.stockQuantity > 0 ? "In Stock" : "Out of Stock"}
-          </h2>
-          <label
-            htmlFor="quantity"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Quantity
-          </label>
-          <select
-            id="quantity"
-            name="quantity"
-            className="mt-1 block w-18 pl-3 pr-10 py-2 text-base border-2 border-black focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
-            value={quantity}
-            onChange={(e) => {
-              setQuantity(e.target.value),
-                setTotalPrice(
-                  product.price * e.target.value + selectedShipping.price
-                );
-            }}
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-          <div className="mt-4">
-            <h3 className="text-lg font-medium mb-2">
-              Choose a shipping option:
-            </h3>
-            <div className="flex gap-4">
-              <button
-                className={`border-2 px-4 py-2 rounded-md transition ${
-                  selectedShipping.date === threeDayArrival.toLocaleDateString()
-                    ? "border-blue-500 bg-blue-100"
-                    : "border-black hover:bg-gray-100"
-                }`}
-                onClick={() => {
-                  setSelectedShipping({
-                    date: threeDayArrival.toLocaleDateString(),
-                    price: 5.99,
-                  }),
-                    setTotalPrice(product.price * quantity + 5.99);
-                }}
-              >
-                <p className="font-semibold">
-                  {threeDayArrival.toLocaleDateString()}
-                </p>
-                <p className="text-gray-600 text-sm">$5.99</p>
-              </button>
-              <button
-                className={`border-2 px-4 py-2 rounded-md transition ${
-                  selectedShipping.date === fiveDayArrival.toLocaleDateString()
-                    ? "border-blue-500 bg-blue-100"
-                    : "border-black hover:bg-gray-100"
-                }`}
-                onClick={() => {
-                  setSelectedShipping({
-                    date: fiveDayArrival.toLocaleDateString(),
-                    price: 2.99,
-                  }),
-                    setTotalPrice(product.price * quantity + 2.99);
-                }}
-              >
-                <p className="font-semibold">
-                  {fiveDayArrival.toLocaleDateString()}
-                </p>
-                <p className="text-gray-600 text-sm">$2.99</p>
-              </button>
-            </div>
-            <div className="mt-2 flex gap-2">
-              <h3 className="text-lg font-medium">Total Price:</h3>
-              <p className="text-xl font-bold">${totalPrice}</p>
-            </div>
-          </div>
-          <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md block">
-            Add to cart
-          </button>
-          <button
-            className="mt-4 bg-orange-500 text-white px-4 py-2 rounded-md"
-            onClick={handleAddOrder}
-          >
-            Buy Now
-          </button>
-          {showQuantityBanner && (
-            <NotificationBanner text="Not enough stock available!" />
-          )}
-          {showOrderBanner && (
-            <NotificationBanner text="Order placed successfully!" />
-          )}
-        </div>
-        <div className="mt-4 border-2 border-black p-4 rounded-md">
-          <label
-            htmlFor="review"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Write a review
-          </label>
-          {showRatingBanner && (
-            <NotificationBanner text="Please select a rating before posting your review." />
-          )}
-          <StarRating rating={rating} setRating={setRating} />
-          <div className="mt-1 flex gap-2">
-            <textarea
-              id="review"
-              name="review"
-              placeholder="What should others know about this product?"
-              className="mt-1 block h-20 w-full pl-3 pr-10 py-2 text-base border-2 border-black focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
-              value={reviewText}
-              onChange={(e) => setReviewText(e.target.value)}
-            ></textarea>
-            <div className="flex justify-center items-end">
-              <button
-                className="h-10 bg-blue-500 text-white px-4 py-2 rounded-md"
-                onClick={handleAddReview}
-              >
-                Post
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="mt-4 border-2 border-black p-4 rounded-md">
-          <h2 className="text-xl font-medium">Reviews</h2>
-          <div className="mt-2">
-            {reviews.map((review, index) => (
-              <div
-                key={index}
-                className="flex flex-col gap-2 border-b border-gray-300 py-2"
-              >
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center gap-2">
-                    <p className="text-sm font-semibold">{review.userName}</p>
-                    <span className="text-yellow-500 text-sm">
-                      ⭐ {review.rating} / 5
-                    </span>
-                  </div>
-                  <p className="text-gray-800 text-sm">{review.createdAt}</p>
-                </div>
-                <div className="flex flex-row justify-between items-center">
-                  <p className="text-gray-600">{review.reviewText}</p>
-                  {/* Trash icon for deleting the review */}
-                  {user?.uid === review.userId && (
-                    <FaTrash
-                      className="text-red-500 cursor-pointer"
-                      onClick={() => handleDeleteReview(product.id, review.id)}
-                    />
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <WriteReview
+          showRatingBanner={showRatingBanner}
+          rating={rating}
+          setRating={setRating}
+          reviewText={reviewText}
+          setReviewText={setReviewText}
+          handleAddReview={handleAddReview}
+        />
+        <Reviews
+          reviews={reviews}
+          product={product}
+          user={user}
+          handleDeleteReview={handleDeleteReview}
+        />
       </div>
     </div>
   );
